ef3909d8b2a323c5fdc570ea490162a6e2a87593
XGG <zapperhatz26@gmail.com>
2018-11-22 10:41:40 +0800

    additional queries

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-	error_reporting(E_ERROR | E_PARSE);
+	// error_reporting(E_ERROR | E_PARSE);
 	require_once 'db.php';
-	$dbHost = "localhost";
-	$dbUser = "root";
-	$dbPass = "root";	
+	// $dbHost = "localhost";
+	// $dbUser = "root";
+	// $dbPass = "root";	
 ?>
-<html>
+<!-- <html>
 <head>
 	<title>Hybrid Report Generation</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<meta author="Wireless Access for Health">
 </head>
-<body>
+<body> -->
 	<br>
 	<div class="container">
 		
@@ -30,7 +30,7 @@
 				  <input type='submit' class="btn btn-outline-primary" name='go' value='Submit'>  	
 		</form>
 		<div class="row">
-			<div class="col-lg-12 col-md-12 col-xs-12 text-center"><div class="alert alert-primary">Technical Statistics</div></div>
+			<div class="col-lg-12 col-md-12 col-xs-12 text-center"><div class="alert alert-danger">Technical Statistics</div></div>
 		</div>
 			<table class='table'>
 				<thead>
@@ -47,7 +47,7 @@
 						{
 
 							$dbName = $_POST['rhu'];
-							$dbConnect = mysql_connect($dbHost,$dbUser,$dbPass);
+							$dbConnect = mysql_connect("192.168.100.11","root","root");
 							mysql_select_db($dbName,$dbConnect);
 
 							$patientCount = "SELECT ( SELECT count(*) FROM patient ) as patient,
@@ -85,7 +85,7 @@
 			<table class='table'>
 				<thead>
 					<tr>
-						<th>NHTS Families Registered</th>
+						<th>NHTS Members Registered</th>
 						<th>PhilHealth Member Count</th>
 						<th>PhilHealth Dependent Count</th>
 						<th>PhilHealth Count</th>
@@ -141,7 +141,60 @@
 			<div class="row">
 				<div class="col-lg-12 col-md-12 col-xs-12 text-center"><div class="alert alert-success">HPP Statistics</div></div>
 			</div>
-			Coming Soon!
+			<table class="table">
+				<tr><th>Invalid Philhealth Numbers</th><th>Empty Family Folder</th><th>Invalid Phone Numbers</th></tr>
+				<?php
+						if (isset($_REQUEST['go']) && $_REQUEST['go'] == 'Submit')
+						{ 
+
+							$patient_philhealth = "SELECT 
+							(SELECT count(*) FROM patient_philhealth where ((length(philhealth_id)!=14) and philhealth_id like '%-%') OR (length(philhealth_id)!=12 and philhealth_id not like '%-%')) AS invalid_philhealth,
+							(0) AS empty_fam,
+							(SELECT count(*) FROM patient WHERE mobile_number = '0000000000' or length(mobile_number) != 10 or mobile_number = '1111111111' or mobile_number='9000000000') AS invalid_phone";
+							// $query2 = $database->_dbQuery($patient_philhealth);
+							// $result2=$database->_dbFetch($query2);
+							//
+							$query2 = mysql_query($patient_philhealth);
+							$total2 = mysql_num_rows($query2);
+							
+							if ($total2 == 0) {
+								$patient_philhealth = "SELECT 
+							(SELECT * FROM m_patient_philhealth where ((length(philhealth_id)!=14) and philhealth_id like '%-%') OR (length(philhealth_id)!=12 and philhealth_id not like '%-%')) AS invalid_philhealth,
+							(SELECT count(*) FROM m_family left join `m_family_members` on m_family.family_id = m_family_members.family_id where patient_id is null) AS empty_fam,
+							(SELECT count(*) FROM m_patient WHERE patient_cellphone = '0000000000' or length(patient_cellphone) != 11 or patient_cellphone = '1111111111' or patient_cellphone='9000000000') AS invalid_phone";
+								// $query2 = $database->_dbQuery($patient_philhealth);
+								// $result2=$database->_dbFetch($query2);
+								$query2 = mysql_query($patient_philhealth);
+								$total2 = mysql_num_rows($query2);
+							}
+
+
+
+							$result2= mysql_fetch_array($query2);
+							echo "<tr>";
+								echo '<td>'.$result2['invalid_philhealth'].'</td>';
+								echo '<td>'.$result2['empty_fam'].'</td>';
+								echo '<td>'.$result2['invalid_phone'].'</td>';
+								
+							echo  "</tr>";
+							
+						}	
+					?>
+			</table>
 	</div>
-</body>
-</html>
+<!-- </body>
+</html> -->
+
+
+
+
+select * from m_family full outer join m_family_members on m_family_members.family_id = m_family.family_id
+
+
+SELECT * FROM m_family
+  LEFT JOIN m_family_members ON  m_family.family_id = m_family_members.family_id
+  WHERE m_family.family_id is null
+  UNION ALL
+  SELECT * FROM m_family
+  RIGHT JOIN m_family_members ON  m_family.family_id = m_family_members.family_id
+  WHERE  m_family.family_id is null
