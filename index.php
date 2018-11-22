cccf1d03d74eaf1a783f19f646daed43b971063e
XGG <zapperhatz26@gmail.com>
2018-11-22 13:47:13 +0800

    fix missing semicolon

--- index.php
+++ index.php
@@ -162,7 +162,7 @@
 								$patient_philhealth = "SELECT 
 							(SELECT count(*) FROM m_patient_philhealth where ((length(philhealth_id)!=14) and philhealth_id like '%-%') OR (length(philhealth_id)!=12 and philhealth_id not like '%-%')) AS invalid_philhealth,
 							(SELECT count(*) FROM m_family left join `m_family_members` on m_family.family_id = m_family_members.family_id where patient_id is null) AS empty_fam,
-							(SELECT count(*) FROM m_patient WHERE patient_cellphone = '0000000000' or length(patient_cellphone) != 11 or patient_cellphone = '1111111111' or patient_cellphone='9000000000') AS invalid_phone;"
+							(SELECT count(*) FROM m_patient WHERE patient_cellphone = '0000000000' or length(patient_cellphone) != 11 or patient_cellphone = '1111111111' or patient_cellphone='9000000000') AS invalid_phone;";
 								// $query2 = $database->_dbQuery($patient_philhealth);
 								// $result2=$database->_dbFetch($query2);
 								$query2 = mysql_query($patient_philhealth);
@@ -197,7 +197,7 @@
 					?>
 			</table>
 	</div>
-<!-- </body>
-</html> -->
+</body>
+</html>
 
 
