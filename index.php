548fc46f7feb4df5ba1777ab292dc92f5a9a036c
XGG <zapperhatz26@gmail.com>
2019-01-03 08:51:57 +0800

    more debugging

--- index.php
+++ index.php
@@ -71,6 +71,10 @@
 							( SELECT count(*) FROM consult where created_at between date('$start_date') and date('$end_date')) as consult,
 							( SELECT count(*) FROM family ) as family,
 							( SELECT sum(population) FROM lib_catchment_barangay) as catchment";
+
+							?>
+<script type="text/javascript">console.log("<?php echo $patientCount; ?>");</script>
+							<?php
 							$query1 = mysql_query($patientCount);
 							// echo "$patientCount";
 							$total1 = mysql_num_rows($query1);
